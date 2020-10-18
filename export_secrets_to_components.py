aac22a2504ac6f351a53b0b623976f02b150c27d
David Aronchick <aronchick@gmail.com>
2020-10-18 23:38:22 +0000

    moving some files to absolute

--- export_secrets_to_components.py
+++ export_secrets_to_components.py
@@ -21,11 +21,15 @@ if not (component_dir.exists()):
 for component in all_components:
     var_name = f"COMPONENT_{component}"
     component_value = os.environ.get(var_name)
+    if component_value is None:
+        print(os.environ)
     file_path = component_dir / f"{var_name}.yaml"
     file_path.write_text(component_value)
 
 cert_name = 'workflow-cert'
-os.popen(f'az keyvault secret download --vault-name workflow-vault --name {cert_name} --file {cert_name}-export.txt')
-os.popen(f'base64 --decode {cert_name}-export.txt > components/{cert_name}.pfx')
+cert_export_file = component_dir / f"{cert_name}.txt"
+cert_final_file = component_dir / f"{cert_name}.pfx"
+os.popen(f'az keyvault secret download --vault-name workflow-vault --name {cert_name} --file {str(cert_export_file)}')
+os.popen(f'base64 --decode {str(cert_export_file.absolute)} > {str(cert_final_file.absolute)}')
 
 os.popen('dapr init')
