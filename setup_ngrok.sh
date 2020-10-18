7b0c44a70a89cb568f51f8663b3ec62e2377d85d
David Aronchick <aronchick@gmail.com>
2020-10-18 22:53:54 +0000

    updating ngrok script

--- setup_ngrok.sh
+++ setup_ngrok.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+
 if [[ -z "$NGROK_TOKEN" ]]; then
   echo "Please set 'NGROK_TOKEN'"
   exit 2
+fi
+
+if [[ -z "$USER_PASS" ]]; then
+  echo "Please set 'USER_PASS' for user: $USER"
+  exit 3
 fi
 
 echo "### Install ngrok ###"
@@ -11,7 +17,11 @@ wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
 unzip ngrok-stable-linux-386.zip
 chmod +x ./ngrok
 
+echo "### Update user: $USER password ###"
+echo -e "$USER_PASS\n$USER_PASS" | sudo passwd "$USER"
+
 echo "### Start ngrok proxy for 22 port ###"
+
 
 rm -f .ngrok.log
 ./ngrok authtoken "$NGROK_TOKEN"
