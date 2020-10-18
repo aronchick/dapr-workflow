b0675585c31bbd9ed9ec33b77c91a655ca07ee99
David Aronchick <aronchick@gmail.com>
2020-10-18 23:33:10 +0000

    moving dapr setup and export to python file

--- export_secrets_to_components.py
+++ export_secrets_to_components.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 all_components = [
     "AZUREKEYVAULT",
@@ -9,9 +10,22 @@ all_components = [
     # "kafka-binding",
     # "localredisstate",
 
+dapr_dir = Path('/home/runner/.dapr') 
+if not (dapr_dir.exists()):
+    dapr_dir.mkdir()
+
+component_dir = dapr_dir / 'components'
+if not (component_dir.exists()):
+    component_dir.mkdir()
+
 for component in all_components:
-    component_value = os.environ.get(f"COMPONENT_{component}")
+    var_name = f"COMPONENT_{component}"
+    component_value = os.environ.get(var_name)
+    file_path = component_dir / f"{var_name}.yaml"
+    file_path.write_text(component_value)
 
 cert_name = 'workflow-cert'
 os.popen(f'az keyvault secret download --vault-name workflow-vault --name {cert_name} --file {cert_name}-export.txt')
-os.popen(f'base64 --decode {cert_name}.txt > components/{cert_name}.pfx')
+os.popen(f'base64 --decode {cert_name}-export.txt > components/{cert_name}.pfx')
+
+os.popen('dapr init')
