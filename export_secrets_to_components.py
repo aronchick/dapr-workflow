10aaaa945b84d3416fc5dd5252cf43dc63650632
David Aronchick <aronchick@gmail.com>
2020-10-19 17:03:44 +0000

    moving more secret export to python

--- export_secrets_to_components.py
+++ export_secrets_to_components.py
@@ -11,6 +11,7 @@ all_components = [
 # "localredisstate",
 
 component_dir = Path("components")
+print(f"Component dir: {component_dir}")
 if not (component_dir.exists()):
     component_dir.mkdir()
 
@@ -19,7 +20,13 @@ for component in all_components:
     component_value = os.environ.get(var_name)
     if component_value is None:
         print(os.environ)
-    file_path = component_dir / f"{var_name}.yaml"
-    file_path.write_text(component_value)
+        file_path = component_dir / f"{var_name}.yaml"
+        file_path.write_text(component_value)
+
+workflow_cert = os.environ.get('WORKFLOW_CERT')
+cert_txt_path = component_dir / "workflow-cert.txt"
+cert_txt_path.write_text(workflow_cert)
+os.popen(f'base64 --decode {str(cert_txt_path)} > {str(component_dir)}/workflow-cert.pfx')
+
 
 os.popen("dapr init")
