7f31ce0163e19cb1f1f5e0145d7c40d506860b91
David Aronchick <aronchick@gmail.com>
2020-10-19 22:31:42 +0000

    fixing conditional

--- export_secrets_to_components.py
+++ export_secrets_to_components.py
@@ -11,7 +11,7 @@ all_components = [
 # "localredisstate",
 
 component_dir = Path("components")
-# print(f"Component dir: {component_dir}")
+print(f"Component dir: {str(component_dir.absolute)}")
 if not (component_dir.exists()):
     component_dir.mkdir()
 
@@ -19,8 +19,7 @@ for component in all_components:
     var_name = f"COMPONENT_{component}"
     component_value = os.environ.get(var_name)
     print(f"V - {var_name}: {component_value}")
-    if component_value is None:
-        # print(os.environ
+    if component_value is not None:
         file_path = component_dir / f"{var_name}.yaml"
         print(f"Writing to {str(file_path.absolute)}")
         file_path.write_text(component_value)
@@ -28,6 +27,6 @@ for component in all_components:
 workflow_cert = os.environ.get('WORKFLOWVAULTRBACCERT')
 cert_txt_path = component_dir / "WORKFLOWVAULTRBACCERT.txt"
 cert_txt_path.write_text(workflow_cert)
-exec_command = f"base64 -di {str(cert_txt_path)} > {str(component_dir)}/workflowVaultRBACCert.pfx"
+exec_command = f"base64 -di {str(cert_txt_path)} > {str(component_dir)}/WORKFLOWVAULTRBACCERT.pfx"
 print(f"Exec command: {exec_command}")
 os.popen(exec_command)
