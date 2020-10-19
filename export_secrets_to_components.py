0d4d2b2aa67a948bacc3fe3f7884f7fba8716fa4
David Aronchick <aronchick@gmail.com>
2020-10-19 17:19:04 +0000

    updating base64 processing to handle errors better

--- export_secrets_to_components.py
+++ export_secrets_to_components.py
@@ -11,22 +11,26 @@ all_components = [
 # "localredisstate",
 
 component_dir = Path("components")
-print(f"Component dir: {component_dir}")
+# print(f"Component dir: {component_dir}")
 if not (component_dir.exists()):
     component_dir.mkdir()
 
 for component in all_components:
     var_name = f"COMPONENT_{component}"
     component_value = os.environ.get(var_name)
+    # print(f"V - {var_name}: {component_value}")
     if component_value is None:
-        print(os.environ)
+        # print(os.environ
         file_path = component_dir / f"{var_name}.yaml"
+        print(f"Writing to {str(file_path.absolute)}")
         file_path.write_text(component_value)
 
 workflow_cert = os.environ.get('WORKFLOW_CERT')
 cert_txt_path = component_dir / "workflow-cert.txt"
 cert_txt_path.write_text(workflow_cert)
-os.popen(f'base64 --decode {str(cert_txt_path)} > {str(component_dir)}/workflow-cert.pfx')
+exec_command = f"base64 -di {str(cert_txt_path)} > {str(component_dir)}/workflow-cert.pfx"
+print(f"Exec command: {exec_command}")
+os.popen(exec_command)
 
 
 os.popen("dapr init")
